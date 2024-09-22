cff71922338a1443ff6efcc020badd4727773b01
qianyueh <h20041124@gmail.com>
2024-09-23 02:01:51 +0800

    Update web app with enhanced vibrato and other improvements

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "e8627c559067405d7b225e428c0691be",
+"flutter_bootstrap.js": "0ed98c4fea8b15c4dc70ab3d3389e1e1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c8fa4d35b4022955cd552e7737565720",
 "/": "c8fa4d35b4022955cd552e7737565720",
-"main.dart.js": "ccab46984772b084a90a7ada13a3a43a",
+"main.dart.js": "bbcd975ade14c5c576b17bf5af3b35a5",
 "manifest.json": "f6a9b4b1bd35f986d2cca645e250012a",
 "version.json": "c8165f082ac83e9eea29e969b2765c03"};
 // The application shell files that are downloaded before a service worker can
